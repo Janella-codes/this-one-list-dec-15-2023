66501aac0f13827bc32f44124746ddd9ad9ef71b
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2023-12-16 08:23:18 -0800

    Add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={`${inter.className}  bg-cyan-200 text-slate-600 container mx-auto p-4`}>{children}</body>
+       <body className={`${inter.className}  bg-cyan-500 text-slate-600 container mx-auto p-4`}>{children}</body>
     </html>
   )
 }
